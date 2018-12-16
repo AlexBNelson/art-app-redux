ca3846738df950da93af9bc3efc8b71e12e1010b
Nelson Alexander <alexbnelson@yahoo,com>
2018-12-16 16:50:05 +0000

    changed urls

--- src/components/Article/ArticlePane/ImagePane/ImagePaneComponent.js
+++ src/components/Article/ArticlePane/ImagePane/ImagePaneComponent.js
@@ -17,7 +17,7 @@ class ImagePaneComponent extends Component {
     componentDidMount() {
         var imageLinks = [];
 
-        const imageLinksUrl = 'http://localhost:58282/articleContent/' + this.props.id + '/ImageLinks';
+        const imageLinksUrl = 'https://dyptych-fa.azurewebsites.net/api/' + this.props.id + '/ImageLinks';
 
         axios({
             method: 'get',
--- src/components/Article/ArticlePane/TextPane.js
+++ src/components/Article/ArticlePane/TextPane.js
@@ -32,7 +32,7 @@ class TextPane extends Component {
         const { hideImage, dispatch } = this.props;
         const scrollNode = this.myScroll;
         
-        const scrollPointUrl = 'http://localhost:58282/articleContent/' + this.props.id + '/ImagePositions';
+        const scrollPointUrl = 'https://dyptych-fa.azurewebsites.net/api/' + this.props.id + '/ImagePositions';
         
         var scrollPoints = [];
         
@@ -56,7 +56,7 @@ class TextPane extends Component {
 
         var imageLinks = [];
 
-        const imageLinksUrl = 'http://localhost:58282/articleContent/' + this.props.id + '/ImageLinks';
+        const imageLinksUrl = 'https://dyptych-fa.azurewebsites.net/api/' + this.props.id + '/ImageLinks';
 
         axios({
             method: 'get',
@@ -132,7 +132,7 @@ class TextPane extends Component {
                 
             });
 
-        const chaptersUrl = 'http://localhost:58282/articleContent/' + this.props.id + '/Chapters';
+        const chaptersUrl = 'https://dyptych-fa.azurewebsites.net/api/' + this.props.id + '/Chapters';
 
         axios({
             method: 'get',
