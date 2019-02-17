a9a483180e4956152e4e53e818fcaaa70cc9002d
Nelson Alexander <alexbnelson@yahoo,com>
2019-02-17 12:55:45 +0000

    Changes article href

--- src/components/InDepth.js
+++ src/components/InDepth.js
@@ -29,7 +29,7 @@ class InDepth extends Component {
                 </div>
                 <div class="row">
                     <div class="col-4 featured-article-title-box feature-article-div">
-                        <a class="dropdown-item" href="/Article/011a8a2b-85ed-4db1-818b-2a7664b5465e">Exploring Exekias</a>
+                        <a class="dropdown-item" href="/Article/c0ee64d3-14c4-41cc-9548-c371118c32ab">Exploring Exekias</a>
 
                     </div>
                     <div class="col-8 featured-article-container feature-article-div">
