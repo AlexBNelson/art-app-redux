850a5f93064c17e562b83b5ee6a1bf508e8d32d8
Nelson Alexander <alexbnelson@yahoo,com>
2019-02-24 16:08:34 +0000

    sdf

--- src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
+++ src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
@@ -184,7 +184,7 @@ class TextPane extends Component {
     render() {
         var introText
 
-        if (this.props.imageSource==1) {
+        if (this.props.imageSource==0) {
             introText = this.state.intro;
         }
 
