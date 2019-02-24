bb54f6901174244d7c4d45b1c5a72d6f7377191b
Nelson Alexander <alexbnelson@yahoo,com>
2019-02-24 20:42:44 +0000

    change to infopane

--- src/components/Article/InfoPane/InfoPane.js
+++ src/components/Article/InfoPane/InfoPane.js
@@ -57,7 +57,7 @@ class InfoPane extends Component {
 
 
             for (i = 0; i < formattedArray.length; i++) {
-                htmlArray.push(<p>{formattedArray[i]}</p>)
+                htmlArray.push(<div>{formattedArray[i]}</div>)
             }
         }
 
