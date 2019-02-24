8037ef4e98e3504b8a5333b924c8167cb9916d3b
Nelson Alexander <alexbnelson@yahoo,com>
2019-02-24 16:17:34 +0000

    fixes to background iamge display mlogic

--- src/components/Article/BackgroundPane/BackgroundPaneContainer.js
+++ src/components/Article/BackgroundPane/BackgroundPaneContainer.js
@@ -6,9 +6,13 @@ const getScrollValue = (value) => {
     return value;
 }
 
+const getImageSource = (source) => {
+    return source;
+}
 
 const mapStateToProps = state => ({
-    articleScrollValue: getScrollValue(state.articleScrollValue)
+    articleScrollValue: getScrollValue(state.articleScrollValue),
+    imageSource: getImageSource(state.imagePaneSource)
 })
 
 export default connect(
