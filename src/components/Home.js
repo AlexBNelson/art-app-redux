ddf869bbdbfeadf6309b3ca7f2ba12e79e50ac09
Nelson Alexander <alexbnelson@yahoo,com>
2019-02-26 16:57:18 +0000

    further development of of article thumbnail

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,16 +46,22 @@ class Home extends Component {
             <div class="container-fluid home-container">
                 <div class="row">
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                     <div class="col-lg-2">
+                        <ArticleThumbnailComponent id='c0ee64d3-14c4-41cc-9548-c371118c32ab'/>
                     </div>
                 </div>
                     </div>
--- src/components/Home/ArticleThumbnailComponent.js
+++ src/components/Home/ArticleThumbnailComponent.js
@@ -1,9 +1,8 @@
 import { Location, Locations } from 'react-router-component';
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import axios from 'axios'
-import '../App.css';
-import '../bootstrap.css'
+import '../../App.css';
+import '../../bootstrap.css'
 
 
 
@@ -19,7 +18,7 @@ class ArticleThumbnailComponent extends Component {
 
     componentDidMount() {
 
-        const imageLinkUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/ImagePositions/0';
+        const imageLinkUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + 'Url?code=zF45E2dH8FYsKMHo6I3wo51lw0BX8a8h5/n9hC51n04VBoQFNg/f0w==';
 
         axios({
             method: 'get',
@@ -28,16 +27,14 @@ class ArticleThumbnailComponent extends Component {
                 "Authorization": "09627a2d93144d10828042019f504b06"
             }
         })
-            .then(function (response) {
-                
-            }
+            .then(response => this.setState({ imageLink: response.data })
             )
             .catch(function (error) {
                 window.alert(error);
 
             });
 
-        const articleTitleUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/ImagePositions/0';
+        const articleTitleUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + 'Title?code=zF45E2dH8FYsKMHo6I3wo51lw0BX8a8h5/n9hC51n04VBoQFNg/f0w==';
 
         axios({
             method: 'get',
@@ -46,9 +43,7 @@ class ArticleThumbnailComponent extends Component {
                 "Authorization": "09627a2d93144d10828042019f504b06"
             }
         })
-            .then(function (response) {
-
-            }
+            .then(response => this.setState({ articleTitle: response.data })
             )
             .catch(function (error) {
                 window.alert(error);
@@ -59,9 +54,9 @@ class ArticleThumbnailComponent extends Component {
 
     render() {
         return (
-            <div>
-                <a><img src={this.state.imageLink} /></a>
-                <div></div>
+            <div class="thumbnail-container">
+                <a><img class="thumbnail-image" src={this.state.imageLink} /></a>
+                <div class="thumbnail-title">{this.state.articleTitle}</div>
             </div>
         )
     }
