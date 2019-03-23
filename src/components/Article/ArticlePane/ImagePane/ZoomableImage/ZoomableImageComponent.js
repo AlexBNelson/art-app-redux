836fb59cc78fff61a8965a943a6059b0d653f2dc
Nelson Alexander <alexbnelson@yahoo,com>
2019-03-23 20:09:30 +0000

    Further development on image drag and zoom functionality

--- src/components/Article/ArticlePane/ImagePane/ZoomableImage/ZoomableImageComponent.js
+++ src/components/Article/ArticlePane/ImagePane/ZoomableImage/ZoomableImageComponent.js
@@ -28,13 +28,44 @@ class ZoomableImageComponent extends Component {
 
     }
 
+    onMouseDown(event) {
+
+    }
+
+    onMouseUp(event) {
+
+    }
+
+    onDrag(react) {
+
+    }
+
     render() {
+        var zoomInDisabled
+
+        var zoomOutDisabled
+
+        if (this.state.zoomState == 0) {
+            zoomInDisabled == true;
+        }
+        else {
+            zoomInDisabled == false;
+        }
+
+        if (this.state.zoomState == 2) {
+            zoomOutDisabled == true;
+        }
+        else {
+            zoomOutDisabled == false;
+        }
+
+        var className = "zoo-img-" + this.state.zoomState;
 
         return (
-            <div class="article-image-pane-img">
-                <button class="img-zoom-btn" onClick={this.zoomIn.bind(this)}><img class="button-img" src={arrowRight}></img></button>
-                <button class="img-zoom-btn" onClick={this.zoomOut.bind(this)}><img class="button-img" src={arrowRight}></img></button>
-                <img src={this.props.id} />
+            <div className={className}>
+                <button class="img-zoom-btn" disabled={zoomInDisabled} onClick={this.zoomIn.bind(this)}><img class="button-img" src={arrowRight}></img></button>
+                <button class="img-zoom-btn" disabled={zooOutDisabled} onClick={this.zoomOut.bind(this)}><img class="button-img" src={arrowRight}></img></button>
+                <img  onDrag={this.onDrag} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} src={this.props.id} />
                 </div>
             )
     }
