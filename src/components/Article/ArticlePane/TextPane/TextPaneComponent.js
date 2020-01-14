bb6ec64d6fd36c92333f0ee2d90dd8f49dacf7fe
Nelson Alexander <alexbnelson@yahoo,com>
2020-01-14 22:56:33 +0000

    preliminary work on rendering new model

--- src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
+++ src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
@@ -33,110 +33,9 @@ class TextPane extends Component {
 
 
     componentDidMount() {
-        const { hideImage, dispatch } = this.props;
         const scrollNode = this.myScroll;
 
-        const scrollPointUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/ImagePositions/0';
-
-        var scrollPoints = [];
-
-        var infoUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/ArticleInfo/0'
-
-        axios({
-            method: 'get',
-            url: infoUrl
-        })
-            .then(response => this.setState({ info: response.data.split("\",\"") })
-            )
-            .catch(function (error) {
-                window.alert(error);
-
-            });
-
-        axios({
-            method: 'get',
-            url: scrollPointUrl,
-            headers: {
-                "Authorization": "09627a2d93144d10828042019f504b06"
-            }
-        })
-            .then(function (response) {
-                var str1 = response.data.replace("]", "")
-                var str2 = str1.replace("[", "")
-                var array = str2.split(",")
-                var i;
-
-                for (i = 0; i < array.length; i++) {
-
-                    scrollPoints.push(array[i])
-
-                }
-
-            }
-            )
-            .catch(function (error) {
-                window.alert(error);
-
-            });
-
-        var imageLinks = [];
-
-        const imageLinksUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/ImageLinks/0';
-
-        axios({
-            method: 'get',
-            url: imageLinksUrl,
-            headers: {
-                "Authorization": "09627a2d93144d10828042019f504b06"
-            }
-        })
-            .then(function (response) {
-                var i;
-
-                for (i = 0; i < response.data.length; i++) {
-                    imageLinks.push(response.data[i])
-
-                }
-            }
-            )
-            .catch(function (error) {
-                window.alert(error);
-
-            });
-
         scrollNode.addEventListener('scroll', function () { })
-
-        const introUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/Intro/0';;
-
-
-        axios({
-            method: 'get',
-            url: introUrl,
-            headers: {
-                "Authorization": "09627a2d93144d10828042019f504b06"
-            }
-        })
-            .then(response => this.setState({ intro: response.data })
-            )
-            .catch(function (error) {
-                window.alert(error);
-
-            });
-
-        var chaptersUrl = 'https://dyptychfa2.azurewebsites.net/api/' + this.props.id + '/Chapters/0';
-        axios({
-            method: 'get',
-            url: chaptersUrl,
-            headers: {
-                "Authorization": "09627a2d93144d10828042019f504b06",
-                "Accept": "text/html"
-            }
-        })
-            .then(response => this.setState({ chapters: response.data }))
-            .catch(function (error) {
-                window.alert(error);
-
-            });
     }
 
 
@@ -164,64 +63,31 @@ class TextPane extends Component {
     render() {
         var introText
 
-        var displayedInfo
+        var data = this.props.articleData
+       
+        if(this.props.imageSource==0){
 
-        var infoHtml
+            var introBody = [];
 
-        if (this.props.viewState == false && this.props.imageSource == 0) {
-            displayedInfo = this.state.info[this.state.info.length - 1]
-        }
-        else {
-            displayedInfo = this.state.info[this.props.imageSource - 1]
+            for (var i = 0; i < data.introPage.body.length; i++) {
+                var para;
+                var body = data.introPage.body[i]
+                for (var j = 0; j < body.elements.length; j++) {
+                    var element = body.elements[j];
 
-        }
-
-        if (displayedInfo != null) {
-            displayedInfo.replace("[\"", "")
-            var formattedArray = displayedInfo.split('|');
-            var i = 0;
-            infoHtml = [];
-
-
-            for (i = 0; i < formattedArray.length; i++) {
-                if (formattedArray[i][0] == 'L') {
-                    var link = formattedArray[i].replace('Link: ', '')
-                    infoHtml.push(<div class="info-title"><a href={link}>{link}</a></div>)
+                    switch(element.style){
+                        case 1:
+                            break;
+                        default:
+                    }
                 }
-                else if (formattedArray[i][0] == 'A') {
-                    var link = formattedArray[i].replace('Artist: ', '')
-                    infoHtml.push(<div class="info-title">{link}</div>)
-                }
-                else if (formattedArray[i][0] == 'D') {
-                    var link = formattedArray[i].replace('Date of Image Creation: ', '')
-                    infoHtml.push(<div class="info-title">{link}</div>)
-                }
-                else { //in case of title-first char logic not working properly
-                    var formatted = formattedArray[i].replace('Title: ', '')
-                    var formattedFinal = formatted.replace('Date of Image Creation: ', '')
-                    infoHtml.push(<div class="info-title">{formattedFinal}</div>)
-                }
-
+                
             }
+
+        introText = <div><p>{data.introPage.title}</p><p>{data.introPage.title}</p><p>{data.introPage.}</p></div>
+
         }
 
-        if (this.props.imageSource == 0) {
-            introText = this.state.intro;
-        }
-
-
-        var array = this.state.chapters.split('%');
-        var i = 0;
-        var textArray = [];
-
-        for (i = 0; i < array.length; i++) {
-            var paraList = array[i].split('|');
-            var htmlElement = [];
-            var j = 0;
-            for (j = 0; j < paraList.length; j++) {
-                htmlElement.push(<p>{paraList[j]}</p>)
-            }
-            textArray.push(htmlElement)
         }
 
         return (
