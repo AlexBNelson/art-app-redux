98576af274044eb685ec8cb57e69a4bfe9f0cb68
Nelson Alexander <alexbnelson@yahoo,com>
2020-04-11 17:56:31 +0100

    various styling changes

--- src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
+++ src/components/Article/ArticlePane/TextPane/TextPaneComponent.js
@@ -172,7 +172,7 @@ class TextPane extends Component {
                         <PageIndicatorContainer shift={false}  class="page-indicator" page={this.props.imageSource + 1} total={data.bodyPages.length+2}/>
                         <div class="article-header-date" style={{top: (1.5 + headerOffset) + "vh"}}>{data.bodyPages[this.props.imageSource - 1].date}</div>
                         <div class="article-header-medium" style={{top: (2.5 + headerOffset) + "vh"}}>{data.bodyPages[this.props.imageSource - 1].medium}</div>
-                        <div class="article-header-museum"style={{top: (3.5 + headerOffset) + "vh"}}>{data.bodyPages[this.props.imageSource - 1].museum}<img class="cc-logo" alt="Image Credits" src={creativeCommons}/></div>
+                        <div class="article-header-museum"style={{top: (3.5 + headerOffset) + "vh"}}>{data.bodyPages[this.props.imageSource - 1].museum}</div>
                       </div>)
             }
             else{
